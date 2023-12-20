1062ca3a2640ec8227a92242b1c76b71d29c5a28
kavygan <143586850+kavygan@users.noreply.github.com>
2023-12-20 17:53:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5001;
+
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => console.log('server running on port: http://localhost:${PORT}'))
