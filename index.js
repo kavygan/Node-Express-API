2ed0f143c659c6f9a0c7309ac24bf8212e744521
kavygan <143586850+kavygan@users.noreply.github.com>
2023-12-21 18:17:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
-const PORT = 5001;
+const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.use('/users', usersRoutes);
 
-app.listen(PORT, () => console.log('server running on port: http://localhost:${PORT}'))
+app.get('/', (req, res) =>res.send('hello from homepage.'));
+
+app.listen(PORT, () => console.log('server Running on port: http://localhost:${PORT}'))
+
+
